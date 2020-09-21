354aab73c1a86ef25f4474b05f559e20c2a5cdf7
Vinicius Fantinatto <vifanti@gmail.com>
2020-09-21 20:23:05 -0300

    correçao api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://192.168.1.239:3333",
-  baseURL: "https://ecoleta-fantinatto.herokuapp.com/",
+  baseURL: "https://ecoleta-fantinatto.herokuapp.com",
 });
 
 export default api;
