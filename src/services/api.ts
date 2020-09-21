8c418b7ac41dbd1e76df8bf38abf4acbb0dbed28
Vinicius Fantinatto <vifanti@gmail.com>
2020-09-21 01:52:23 -0300

    funcionalidades avançadas

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
-import exios from "axios";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.239:3333",
+  // baseURL: "http://192.168.1.239:3333",
+  baseURL: "https://ecoleta-fantinatto.herokuapp.com/",
 });
 
 export default api;
